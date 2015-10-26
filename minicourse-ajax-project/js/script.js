8438d87c4ffec1ae2291e126cf80b305fd811bee
Sim Virdi <simeransvirdi@gmail.com>
2015-10-26 16:30:32 +0000

    Background image updated to address

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -15,6 +15,16 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
+    var street = $("#street").val();
+    var city = $("#city").val();  
+    var address = street +', ' + city;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetView = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
+
+    $body.append('<img class="bgimg" src="' + streetView +'">');
+
     return false;
 };
 
