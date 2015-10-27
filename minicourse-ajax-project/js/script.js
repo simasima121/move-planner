c7411ef1bfbd841840ed95cc7bd0fc3650d1bca4
Sim Virdi <simeransvirdi@gmail.com>
2015-10-27 22:58:15 +0000

    Creating NYT API implementation

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -17,13 +17,28 @@ function loadData() {
 
     var street = $("#street").val();
     var city = $("#city").val();  
-    var address = street +', ' + city;
+    var address = street +','+ city;
 
     $greeting.text('So, you want to live at ' + address + '?');
 
     var streetView = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
-
     $body.append('<img class="bgimg" src="' + streetView +'">');
+
+    var nytimesKey = "&sort=newest&api-key=937856a24e65c088446cc0d97b63a8f9:11:73309782"
+    var nytimesAPI = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+city+nytimesKey;
+
+    $.getJSON( nytimesAPI, function (data) {
+        //console.log(data);
+        $nytHeaderElem.text('New York Times Article About ' + city);
+        articles = data.response.docs;
+        $.each( articles, function (key, value) {
+            var link = value.web_url;
+            var headline = value.headline.main;
+            var snip = value.snippet;
+            //console.log(link, headline);
+            $nytElem.append("<li class='article'><a href="+link+">"+headline+"</a><p>"+snip+"</p></li>");
+        });        
+    });
 
     return false;
 };
